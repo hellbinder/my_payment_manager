8f74225787d03976c041c9fe5a6fb2a1cfd35373
name <email>
2014-02-18 17:34:49 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyPaymentManager::Application.config.secret_key_base = '78319ef2190ca71227ee9a878b8ee100a5fa9d0299f04ec5ccd3ccdb59e2104490d27fedcfb2fc8b6347f6b8c89134c7e26d69cf91bc7d622802258cb544dc00'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyPaymentManager::Application.config.session_store :cookie_store, key: '_my_payment_manager_session'
